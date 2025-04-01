ce720c2f87215ec2cba41420b40e087115d990a0
ChamithEC <chamithwijesooriya@gmail.com>
2025-04-01 22:15:39 +0530

    Update auth.ts

--- task-tracker-frontend/src/api/auth.ts
+++ task-tracker-frontend/src/api/auth.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const BASE_API_URL = "http://localhost:3000";
+export const BASE_API_URL = process.env.REACT_APP_BASE_API_URL || "http://localhost:3000";
 
 export interface UserResponse {
   id: number;
